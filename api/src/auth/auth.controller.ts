89217b687073f087017e7df4e347d9295ec3badc
Yassine Hebbat <yhebbat@e2r12p12.1337.ma>
2022-12-15 04:37:57 +0100

    auth without jwt

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('login')
-  login() {
-    return this.authService.login();
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    // console.log({ dto });
+    return this.authService.signup(dto);
   }
 
-  @Post('logout')
-  logout() {
-    return this.authService.logout();
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
   }
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.enableCors();
-	await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove any properties that are not defined in the DTO
+    }),
+  );
+  await app.listen(3000);
 }
 bootstrap();
