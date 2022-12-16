804688a166163e157969a43f93d1075769c00074
AMZIL Ayoub <amzil.ayoob@gmail.com>
2022-12-16 16:44:46 +0100

    [MERGE]

--- api/src/chat/chat.gateway.ts
+++ api/src/chat/chat.gateway.ts
@@ -1,22 +1,14 @@
 import {
-<<<<<<< HEAD
     WebSocketGateway,
     SubscribeMessage,
     MessageBody,
     WebSocketServer,
-=======
-  WebSocketGateway,
-  SubscribeMessage,
-  MessageBody,
-  WebSocketServer,
->>>>>>> d8352677ecf4e9d565f2ce4b67b49fcaeb5df490
 } from '@nestjs/websockets';
 import { Server } from 'http';
 import { ChatService } from './chat.service';
 import { Message } from './dto/message.dto';
 
 @WebSocketGateway({
-<<<<<<< HEAD
     cors: {
         origin: '*',
     },
@@ -39,28 +31,4 @@ export class ChatGateway {
     findAll() {
         return this.chatService.findAllMessages();
     }
-=======
-  cors: {
-    origin: '*',
-  },
-})
-export class ChatGateway {
-  @WebSocketServer()
-  server: Server;
-
-  constructor(private readonly chatService: ChatService) {}
-
-  @SubscribeMessage('createMessage')
-  async create(@MessageBody() createMessage: Message) {
-    const message = await this.chatService.create(createMessage);
-    this.server.emit('createMessage', message);
-
-    return message;
-  }
-
-  @SubscribeMessage('findAllMessage')
-  findAll() {
-    return this.chatService.findAll();
-  }
->>>>>>> d8352677ecf4e9d565f2ce4b67b49fcaeb5df490
 }
