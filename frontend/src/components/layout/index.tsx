9abb6353f63317e99b3304010158eec07d319206
f0lio <36214338+f0lio@users.noreply.github.com>
2023-01-10 15:21:54 +0100

    sync: tweak MainLayout

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,49 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import cn from "classnames";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import ChatStuff from "@components/chat/ChatStuff";
 import Navbar from "@components/navbar";
+import { useAuthContext } from "context/auth.context";
 
 interface MainLayoutProps {
   children: React.ReactNode;
   title?: string;
+  noLayout?: boolean;
   backgroundColor?: string;
   pageIsProtected?: boolean;
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({
   children,
-  // pageIsProtected = true,
+  pageIsProtected = true,
+  noLayout = false,
   title = "Transcendence",
   backgroundColor,
 }) => {
-  // const ctx = useAuthContext();
+  const ctx = useAuthContext();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   if (ctx?.isAuthenticated === false && pageIsProtected) {
-  //     router.replace("/");
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (
+      ctx?.isAuthenticated === false &&
+      pageIsProtected &&
+      window.location.pathname !== "/"
+    )
+      router.replace("/");
+  }, []);
 
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-      <Navbar />
-      <main className="flex w-full h-full min-h-screen">
-        <div
-          className={cn(
-            "pt-20 flex flex-col items-center w-full justify-center h-full relative",
-            backgroundColor
-          )}
-        >
-          {children}
-        </div>
-        <ChatStuff />
-      </main>
+      {noLayout ? (
+        <>{children}</>
+      ) : (
+        <>
+          <Navbar />
+          <main className="flex w-full h-full min-h-screen">
+            <div
+              className={cn(
+                "pt-20 flex flex-col items-center w-full justify-center h-full relative",
+                backgroundColor
+              )}
+            >
+              {children}
+            </div>
+            {ctx?.isAuthenticated && <ChatStuff />}
+          </main>
+        </>
+      )}
     </>
   );
 };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { BiHide, BiShowAlt } from "react-icons/bi";
 
+import MainLayout from "@components/layout";
 import Button from "@ui/Button";
 import TextInput from "@ui/TextInput";
 import { LANDING_IMAGE } from "@utils/constants";
@@ -11,7 +12,7 @@ const SiginFields = () => {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <section className="group w-full flex justify-center cursor-wait flex-col relative">
-      <div className="flex-col flex blur-sm gap-y-4 cursor-wait w-full">
+      <div className="flex-col flex group-hover:blur-sm gap-y-4 cursor-wait w-full">
         <TextInput
           label="Username or Email"
           placeholder="Username or Email"
@@ -35,7 +36,7 @@ const SiginFields = () => {
             {showPassword ? <BiHide /> : <BiShowAlt />}
           </button>
         </div>
-        <Button onClick={() => {}} className="min-w-full">
+        <Button onClick={() => {}} className="min-w-full cursor-wait">
           Sign In
         </Button>
       </div>
@@ -47,42 +48,57 @@ const SiginFields = () => {
 };
 
 export default function Home() {
-  return (
-    <div className="grid grid-cols-5 divide-x-2 min-h-screen">
-      <div className="grid xl:col-span-2 items-center justify-center col-span-5">
-        <section className="flex flex-col items-center px-10 py-16 gap-y-8 shadow-lg max-w-md border rounded-xl p-6">
-          <header className="flex items-center flex-col gap-y-2">
-            <h1 className="text-3xl font-bold">Sign in to Pong</h1>
-            <p className="text-gray-500">
-              Login to play Pong with your friends
-            </p>
-          </header>
-          <SiginFields />
-          <p className="text-gray-500">
-            <span className="h-px mb-1 w-32 bg-gray-200 inline-block" />
-            <span className="mx-2">OR</span>
-            <span className="h-px mb-1 w-32 bg-gray-200 inline-block" />
-          </p>
+  const handle42Login = async () => {
+    window.location.href = process.env.NEXT_PUBLIC_API_URL + "/auth/login42"; 
+  };
 
-          <button
-            type="button"
-            className="flex items-center justify-center gap-x-4 px-10 py-2 rounded-xl bg-orange-500
-              hover:scale-105 hover:bg-orange-600 transition duration-300 ease-in-out w-full
-            "
-          >
-            <Image src={"/42-logo.svg"} width={36} height={36} alt="42 logo" />
-            <p className="pb-1 font-semibold text-lg">Sign In with 42</p>
-          </button>
-        </section>
+  return (
+    <MainLayout noLayout>
+      <div className="grid grid-cols-5 divide-x-2 min-h-screen">
+        <div className="grid xl:col-span-2 items-center justify-center col-span-5">
+          <section className="flex flex-col items-center px-14 py-24 gap-y-8 shadow-lg max-w-md border rounded-xl">
+            <header className="flex items-center flex-col gap-y-2">
+              <h1 className="text-3xl font-bold">Sign in to Pong</h1>
+              <p className="text-gray-500">
+                Login to play Pong with your friends
+              </p>
+            </header>
+            <SiginFields />
+            <p className="text-gray-500">
+              <span className="h-px mb-1 w-32 bg-gray-200 inline-block" />
+              <span className="mx-2">OR</span>
+              <span className="h-px mb-1 w-32 bg-gray-200 inline-block" />
+            </p>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                handle42Login();
+              }}
+              type="button"
+              className="flex items-center justify-center gap-x-4 px-10 py-2 rounded-xl bg-orange-500
+              hover:scale-105 hover:bg-orange-600 transition duration-300 ease-in-out w-full"
+            >
+              <Image
+                src={"/42-logo.svg"}
+                width={36}
+                height={36}
+                alt="42 logo"
+              />
+              <p className="pb-1 font-semibold text-white text-lg">
+                Sign In with 42
+              </p>
+            </button>
+          </section>
+        </div>
+        <figure className="sm:col-span-3 hidden xl:block">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={LANDING_IMAGE}
+            alt="Pong image"
+            className="object-cover w-full h-full max-h-screen"
+          />
+        </figure>
       </div>
-      <figure className="sm:col-span-3 hidden xl:block">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={LANDING_IMAGE}
-          alt="Pong image"
-          className="object-cover w-full h-full max-h-screen"
-        />
-      </figure>
-    </div>
+    </MainLayout>
   );
 }
