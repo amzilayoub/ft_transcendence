e234805ce8e015d15d7624a7aac9b50db17e2fbf
f0lio <36214338+f0lio@users.noreply.github.com>
2023-01-11 14:47:51 +0100

    reduce size/and req time for landing image

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,4 @@
 export const FALLBACK_AVATAR = "/images/default-avatar.jpg";
 export const FALLBACK_COVER = "/images/default-cover.png";
 export const APP_NAME = "LGTEAM";
-export const LANDING_IMAGE =
-  "https://images.unsplash.com/photo-1583634852966-130c15654305";
+export const LANDING_IMAGE = "/images/supermario-arcade-fighters.jpeg";
