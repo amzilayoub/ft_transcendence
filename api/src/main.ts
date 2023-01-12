59f0f01fc54395379d321b6e3b76d937803f0b50
AMZIL Ayoub <amzil.ayoob@gmail.com>
2023-01-12 20:29:08 +0100

    [IMP] adding swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-import { join } from 'path';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -15,6 +15,14 @@ async function bootstrap() {
         }),
     );
     app.use(cookieParser());
+    const config = new DocumentBuilder()
+        .setTitle('transcendence')
+        .setDescription('The transcendence API description')
+        .setVersion('1.0')
+        .addTag('transcendence')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
     await app.listen(3000);
 }
 bootstrap();
