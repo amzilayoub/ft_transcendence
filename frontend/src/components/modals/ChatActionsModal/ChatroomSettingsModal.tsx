00016c68154012b29c9bc45c1f44da1360be84f5
Mohamed Bifenzi <mbifenzi@e2r2p12.1337.ma>
2023-01-23 23:21:51 +0100

    room settings improved significantly

--- frontend/src/components/modals/ChatActionsModal/ChatroomSettingsModal.tsx
+++ frontend/src/components/modals/ChatActionsModal/ChatroomSettingsModal.tsx
@@ -5,11 +5,44 @@ import BaseModal from "@ui/BaseModal";
 import UserListItemLoading from "@ui/skeletons/UserSkeletons";
 import TextInput from "@ui/TextInput";
 import cn from "classnames";
-import { IConversation, IRoom, IRoomMember } from "global/types";
+import {
+  IConversation,
+  IRoom,
+  IRoomMember,
+  MemberGameStatus,
+  MembershipStatus,
+} from "global/types";
 import Image from "next/image";
-import { IoSearchOutline } from "react-icons/io5";
+import { BiBlock } from "react-icons/bi";
+import { FaRegEdit } from "react-icons/fa";
+import { IoPersonRemoveOutline, IoSearchOutline } from "react-icons/io5";
+import { RiVolumeMuteLine } from "react-icons/ri";
 
 const MemberListItem = ({ member }: { member: IRoomMember }) => {
+  const CurrentUser: IRoomMember = {
+    id: 1,
+    username: "mbif",
+    avatar_url: "https://i.imgur.com/0y0tj9X.png",
+    isOnline: true,
+    gameStatus: MemberGameStatus.IDLE,
+    membershipStatus: MembershipStatus.OWNER,
+    isBanned: false,
+    isMuted: false,
+    mutedUntil: new Date(),
+  };
+
+  const handleMute = () => {
+    console.log("mute");
+  };
+
+  const handleBlock = () => {
+    console.log("block");
+  };
+
+  const handleKick = () => {
+    console.log("kick");
+  };
+
   return (
     <li
       className={cn(
@@ -21,16 +54,37 @@ const MemberListItem = ({ member }: { member: IRoomMember }) => {
     >
       <div className="flex items-center gap-x-2 justify-between w-full">
         <div className="w-full flex justify-between gap-x-2">
-          <div className="ml-2 flex justify-between items-center">
-            <Image
-              src={"/images/default-avatar.jpg"}
-              alt={member + " avatar"}
-              width={40}
-              height={40}
-              className="rounded-full"
-            />
-            <div className="ml-2">
-              <p className="text-sm font-medium">{member.username}</p>
+          <div className="w-full ml-2 flex flex-row justify-between items-center ">
+            <div>
+              <div className="ml-2 flex items-center gap-4 w-32">
+                <Image
+                  src={"/images/default-avatar.jpg"}
+                  alt={member + " avatar"}
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                />
+                <p className="text-sm font-medium">{member.username}</p>
+              </div>
+            </div>
+            <div>
+              <p className="text-sm text-gray-400">{member.membershipStatus}</p>
+            </div>
+            <div className="flex gap-2">
+              <RiVolumeMuteLine
+                onClick={handleMute}
+                className="text-2xl text-red-800 bg-gray-200 hover:bg-gray-300 duration-300 rounded-full p-1 h-8 w-8"
+              />
+              <BiBlock
+                onClick={handleBlock}
+                className="text-2xl text-red-800 bg-gray-200 hover:bg-gray-300 duration-300 rounded-full p-1 h-8 w-8"
+              />
+              {CurrentUser.membershipStatus === MembershipStatus.OWNER && (
+                <IoPersonRemoveOutline
+                  onClick={handleKick}
+                  className="text-2xl text-red-800 bg-gray-200 hover:bg-gray-300 duration-300 rounded-full p-1 h-8 w-8"
+                />
+              )}
             </div>
           </div>
         </div>
@@ -52,14 +106,79 @@ const ChatroomSettingsModal = ({
   const searchError = false;
   const searchLoading = false;
   const [searchQuery, setSearchQuery] = useState("");
+  const [showUsernameModal, setShowUsernameModal] = useState(false);
   const searchResults = roomData.members.filter((member: IRoomMember) =>
     member.username.toLowerCase().includes(searchQuery.toLowerCase())
   );
+  const CurrentUser: IRoomMember = {
+    id: 1,
+    username: "mbif",
+    avatar_url: "https://i.imgur.com/0y0tj9X.png",
+    isOnline: true,
+    gameStatus: MemberGameStatus.IDLE,
+    membershipStatus: MembershipStatus.OWNER,
+    isBanned: false,
+    isMuted: false,
+    mutedUntil: new Date(),
+  };
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
     setShouldSearch(false);
   };
+
+  const OwnerPanel = ({ roomData }: { roomData: IRoom }) => {
+    const [roomCurrentData, setRoomCurrentData] = useState<IRoom>(roomData);
+    return (
+      <div className="p-8">
+        <h2 className="text-2xl font-bold">Room info</h2>
+        <div className="h-px bg-gray-200 " />
+        <div className="flex justify-between items-center pt-5">
+          <div className="flex flex-row items-center w-3/4">
+            <h2 className="text-lg font-medium">{roomCurrentData.name}</h2>
+            <FaRegEdit
+              onClick={() => {
+                setShowUsernameModal(true);
+              }}
+              className="mr-2 ml-4 cursor-pointer"
+            />
+          </div>
+          <div
+            className="group  w-1/4 bg-black transition rounded-full flex justify-center items-center cursor-pointer"
+            onClick={() => {
+              console.log("clicked");
+            }}
+          >
+            <Image
+              src={"/images/default-avatar.jpg"}
+              width={500}
+              height={500}
+              alt={"ss"}
+              className="rounded-full shadow-inner hover:opacity-50 duration-300"
+            />
+            <h1 className="text-white absolute hidden group-hover:block  duration-300 pointer-events-none">
+              upload a photo
+            </h1>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  const AdminPanel = () => {
+    return (
+      <div>
+        <h2 className="text-2xl text-gray-800">owner</h2>
+      </div>
+    );
+  };
+  const MemberPanel = () => {
+    return (
+      <div>
+        <h2 className="text-2xl text-gray-800">owner</h2>
+      </div>
+    );
+  };
+
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
       <div className="p-8 min-h-[calc(50vh)] w-[25vw] max-h-96">
@@ -88,9 +207,15 @@ const ChatroomSettingsModal = ({
           </Tab.List>
           <Tab.Panels className="mt-2">
             <Tab.Panel>
-              <div className="flex flex-row gap-y-4">
-                <h1 className="text-gray-800 font-bold">{roomData.name}</h1>
-              </div>
+              {CurrentUser.membershipStatus === MembershipStatus.OWNER && (
+                <OwnerPanel roomData={roomData} />
+              )}
+              {CurrentUser.membershipStatus === MembershipStatus.MODERATOR && (
+                <AdminPanel />
+              )}
+              {CurrentUser.membershipStatus === MembershipStatus.MEMBER && (
+                <MemberPanel />
+              )}
             </Tab.Panel>
             <Tab.Panel>
               <div>
