634b5fa2307de403445c818ce7da312740ffae52
Mohamed Bifenzi <mbifenzi@e2r2p10.1337.ma>
2023-01-24 20:15:54 +0100

    room settings on the way

---
+++ frontend/src/components/chat/ChatOwnerPanel.tsx
@@ -0,0 +1,188 @@
+import React, { useState } from "react";
+
+import PasswordModal from "@components/modals/ChatActionsModal/RoomPasswordModal";
+import Button from "@ui/Button";
+import TextInput from "@ui/TextInput";
+import { IRoom, RoomType } from "global/types";
+import Image from "next/image";
+import Select from "react-select";
+
+const OwnerPanel = ({ roomData }: { roomData: IRoom }) => {
+  const [roomCurrentData, setRoomCurrentData] = useState<IRoom>(roomData);
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const options = [
+    { value: "public", label: "Public" },
+    { value: "private", label: "Private" },
+    { value: "protected", label: "Protected" },
+  ];
+  const [roomType, setRoomType] = useState("public");
+
+  const [defaultOption, setDefaultOption] = useState(options[0]);
+  const [currentOption, setCurrentOption] = useState(defaultOption);
+
+  const handleChange = (selectedOption: any) => {
+    console.log(`Selected: ${selectedOption.value}`);
+    setCurrentOption(selectedOption);
+    console.log({ selectedOption });
+    if (selectedOption.value === "protected") {
+      setRoomCurrentData({
+        ...roomCurrentData,
+        type: RoomType.PROTECTED,
+      });
+      setShowPasswordModal(true);
+    } else if (selectedOption.value === "private") {
+      setRoomCurrentData({
+        ...roomCurrentData,
+        type: RoomType.PRIVATE,
+      });
+    } else {
+      setRoomCurrentData({
+        ...roomCurrentData,
+        type: RoomType.PUBLIC,
+      });
+    }
+  };
+
+  const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRoomType(e.target.value);
+  };
+
+  return (
+    <div className="p-8 w-full">
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">Room info</h2>
+        <Select
+          defaultValue={defaultOption}
+          options={options}
+          value={currentOption}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="h-px bg-gray-200 " />
+      <div className="flex flex-row justify-between items-center w-full gap-4">
+        <div className="flex flex-col justify-center items-start gap-4 w-full">
+          <div className="w-5/6">
+            <TextInput
+              label="Room Name"
+              type="text"
+              value={roomCurrentData.name}
+              onChange={(e) => {
+                e.preventDefault();
+                setRoomCurrentData({
+                  ...roomCurrentData,
+                  name: e.target.value,
+                });
+              }}
+              required
+            />
+          </div>
+        </div>
+        <div
+          className="group  w-1/4 bg-black transition rounded-full flex justify-center items-center cursor-pointer"
+          onClick={() => {
+            console.log("clicked");
+          }}
+        >
+          <Image
+            src="/public/images/default-avatar.jpg"
+            width={150}
+            height={150}
+            alt={"ss"}
+            className="rounded-full shadow-inner hover:opacity-50 duration-300"
+          />
+          <h1 className="text-white absolute hidden group-hover:block  duration-300 pointer-events-none">
+            upload a photo
+          </h1>
+        </div>
+      </div>
+      <textarea
+        className="w-full h-24 mt-5 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-primary/90 focus:border-transparent"
+        placeholder="Enter room description"
+        value={roomCurrentData.description}
+        onChange={(e) => {
+          e.preventDefault();
+          setRoomCurrentData({
+            ...roomCurrentData,
+            description: e.target.value,
+          });
+        }}
+      />
+      <div>
+        <input
+          type="radio"
+          name="roomType"
+          value="public"
+          defaultChecked
+          onChange={handleRadioChange}
+        />
+        <label htmlFor="">Public</label>
+        <input
+          type="radio"
+          name="roomType"
+          value="private"
+          onChange={handleRadioChange}
+        />
+        <label htmlFor="">Private</label>
+        <input
+          type="radio"
+          name="roomType"
+          value="protected"
+          onChange={handleRadioChange}
+        />
+        <label htmlFor="">Protected</label>
+        {roomType === "protected" && (
+          <>
+            <label>Password</label>
+            <input
+              type="password"
+              disabled={roomType === "protected" ? false : true}
+            />
+          </>
+        )}
+      </div>
+      {roomCurrentData.type === RoomType.PROTECTED && (
+        <div className="">
+          <h2 className="text-2xl font-bold mt-8">Security</h2>
+          <div className="h-px bg-gray-200 " />
+          <div className="flex flex-col gap-4 mt-4 gap-4">
+            <TextInput
+              label="New Password"
+              type="password"
+              value={roomCurrentData.password}
+              onChange={(e) => {
+                e.preventDefault();
+                setRoomCurrentData({
+                  ...roomCurrentData,
+                  password: e.target.value,
+                });
+              }}
+              required
+            />
+            <TextInput
+              label="Confirm Password"
+              type="password"
+              value={roomCurrentData.password}
+              onChange={(e) => {
+                e.preventDefault();
+                setRoomCurrentData({
+                  ...roomCurrentData,
+                  password: e.target.value,
+                });
+              }}
+              required
+            />
+            <Button variant="secondary" size="small">
+              Change Password
+            </Button>
+            <PasswordModal
+              currentOption={currentOption}
+              setCurrentOption={setCurrentOption}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OwnerPanel;
--- frontend/src/components/modals/ChatActionsModal/ChatroomSettingsModal.tsx
+++ frontend/src/components/modals/ChatActionsModal/ChatroomSettingsModal.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
-import { Tab } from "@headlessui/react";
+import OwnerPanel from "@components/chat/ChatOwnerPanel";
 import BaseModal from "@ui/BaseModal";
+import Button from "@ui/Button";
 import UserListItemLoading from "@ui/skeletons/UserSkeletons";
 import TextInput from "@ui/TextInput";
 import cn from "classnames";
@@ -11,12 +12,14 @@ import {
   IRoomMember,
   MemberGameStatus,
   MembershipStatus,
+  RoomType,
 } from "global/types";
 import Image from "next/image";
 import { BiBlock } from "react-icons/bi";
-import { FaRegEdit } from "react-icons/fa";
 import { IoPersonRemoveOutline, IoSearchOutline } from "react-icons/io5";
 import { RiVolumeMuteLine } from "react-icons/ri";
+
+import PasswordModal from "./RoomPasswordModal";
 
 const MemberListItem = ({ member }: { member: IRoomMember }) => {
   const CurrentUser: IRoomMember = {
@@ -106,7 +109,6 @@ const ChatroomSettingsModal = ({
   const searchError = false;
   const searchLoading = false;
   const [searchQuery, setSearchQuery] = useState("");
-  const [showUsernameModal, setShowUsernameModal] = useState(false);
   const searchResults = roomData.members.filter((member: IRoomMember) =>
     member.username.toLowerCase().includes(searchQuery.toLowerCase())
   );
@@ -127,21 +129,33 @@ const ChatroomSettingsModal = ({
     setShouldSearch(false);
   };
 
-  const OwnerPanel = ({ roomData }: { roomData: IRoom }) => {
+  const AdminPanel = ({ roomData }: { roomData: IRoom }) => {
     const [roomCurrentData, setRoomCurrentData] = useState<IRoom>(roomData);
+
     return (
-      <div className="p-8">
-        <h2 className="text-2xl font-bold">Room info</h2>
+      <div className="p-8 w-full">
+        <div className="flex justify-between">
+          <h2 className="text-2xl font-bold">Room info</h2>
+          <p className="text-gray-400">{roomCurrentData.type}</p>
+        </div>
         <div className="h-px bg-gray-200 " />
-        <div className="flex justify-between items-center pt-5">
-          <div className="flex flex-row items-center w-3/4">
-            <h2 className="text-lg font-medium">{roomCurrentData.name}</h2>
-            <FaRegEdit
-              onClick={() => {
-                setShowUsernameModal(true);
-              }}
-              className="mr-2 ml-4 cursor-pointer"
-            />
+        <div className="flex flex-row justify-between items-center w-full gap-4">
+          <div className="flex flex-col justify-center items-start gap-4 w-full">
+            <div className="w-5/6">
+              <TextInput
+                label="Room Name"
+                type="text"
+                value={roomCurrentData.name}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setRoomCurrentData({
+                    ...roomCurrentData,
+                    name: e.target.value,
+                  });
+                }}
+                required
+              />
+            </div>
           </div>
           <div
             className="group  w-1/4 bg-black transition rounded-full flex justify-center items-center cursor-pointer"
@@ -150,9 +164,9 @@ const ChatroomSettingsModal = ({
             }}
           >
             <Image
-              src={"/images/default-avatar.jpg"}
-              width={500}
-              height={500}
+              src="/public/images/default-avatar.jpg"
+              width={150}
+              height={150}
               alt={"ss"}
               className="rounded-full shadow-inner hover:opacity-50 duration-300"
             />
@@ -161,13 +175,56 @@ const ChatroomSettingsModal = ({
             </h1>
           </div>
         </div>
-      </div>
-    );
-  };
-  const AdminPanel = () => {
-    return (
-      <div>
-        <h2 className="text-2xl text-gray-800">owner</h2>
+        <textarea
+          className="w-full h-24 mt-5 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-primary/90 focus:border-transparent"
+          placeholder="Enter room description"
+          value={roomCurrentData.description}
+          onChange={(e) => {
+            e.preventDefault();
+            setRoomCurrentData({
+              ...roomCurrentData,
+              description: e.target.value,
+            });
+          }}
+        />
+        {roomCurrentData.type === RoomType.PROTECTED && (
+          <div className="">
+            <h2 className="text-2xl font-bold mt-8">Security</h2>
+            <div className="h-px bg-gray-200 " />
+            <div className="flex flex-col gap-4 mt-4 gap-4">
+              <TextInput
+                label="New Password"
+                type="password"
+                value={roomCurrentData.password}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setRoomCurrentData({
+                    ...roomCurrentData,
+                    password: e.target.value,
+                  });
+                }}
+                required
+              />
+              <TextInput
+                label="Confirm Password"
+                type="password"
+                value={roomCurrentData.password}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setRoomCurrentData({
+                    ...roomCurrentData,
+                    password: e.target.value,
+                  });
+                }}
+                required
+              />
+              <Button variant="secondary" size="small">
+                Change Password
+              </Button>
+              <PasswordModal />
+            </div>
+          </div>
+        )}
       </div>
     );
   };
@@ -179,90 +236,87 @@ const ChatroomSettingsModal = ({
     );
   };
 
+  const RoomMembers = () => {
+    return (
+      <div className="p-8 h-2/3">
+        <h2 className="text-2xl font-bold">Room Members</h2>
+        <div className="h-px bg-gray-200 " />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("search");
+          }}
+          className="group relative h-10 w-full"
+        >
+          <label className="absolute top-3 left-3 flex items-center justify-center text-gray-400">
+            <button type="submit" className="h-full w-full cursor-default">
+              <IoSearchOutline className="group-focus-within:text-secondary group-hover:text-secondary h-6 w-6 text-gray-400" />
+            </button>
+          </label>
+          <TextInput
+            name="search"
+            placeholder="Search for a member"
+            onChange={(e) => handleSearchChange(e)}
+            inputClassName="pl-12 py-[8px] "
+          />
+        </form>
+        <ul className="no-scrollbar mt-4 gap-y-1 flex flex-col overflow-y-scroll h-[calc(60vh-160px)] scroll-smooth">
+          {!searchError &&
+            !searchLoading &&
+            searchResults &&
+            searchResults?.map((member: IRoomMember, index: number) => (
+              <MemberListItem key={index} member={member} />
+            ))}
+          {searchLoading &&
+            [...new Array(6)].map((i) => <UserListItemLoading key={i} />)}
+          {!searchError && searchResults?.length === 0 && (
+            <p className="py-10 text-center text-gray-400">No results found</p>
+          )}
+        </ul>
+      </div>
+    );
+  };
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [buttonText, setButtonText] = useState("Save");
+  const handleSave = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setButtonText("Saving...");
+    // Make API call or perform other logic here
+    setTimeout(() => {
+      setIsLoading(false);
+      setButtonText("Save");
+      console.log("data saved successfully");
+    }, 2000);
+  };
+
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
-      <div className="p-8 min-h-[calc(50vh)] w-[25vw] max-h-96">
-        <Tab.Group>
-          <Tab.List className="flex space-x-1 rounded-md border">
-            {["Room Preferences", "Room members"].map((tab) => (
-              <Tab
-                key={tab}
-                className={({ selected }) =>
-                  cn(
-                    "w-full rounded-lg py-2.5 text-md font-medium leading-5 cursor-pointer",
-                    {
-                      " ring-1 ring-primary/90 cursor-default ring-offset-1 text-primary bg-white shadow":
-                        selected,
-                      "text-gray-400 hover:bg-white/[0.12] hover:text-gray-600":
-                        !selected,
-                      "rounded-r-sm": tab === "Room Preferences",
-                      "rounded-l-sm": tab === "Room members",
-                    }
-                  )
-                }
-              >
-                {tab}
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels className="mt-2">
-            <Tab.Panel>
-              {CurrentUser.membershipStatus === MembershipStatus.OWNER && (
-                <OwnerPanel roomData={roomData} />
-              )}
-              {CurrentUser.membershipStatus === MembershipStatus.MODERATOR && (
-                <AdminPanel />
-              )}
-              {CurrentUser.membershipStatus === MembershipStatus.MEMBER && (
-                <MemberPanel />
-              )}
-            </Tab.Panel>
-            <Tab.Panel>
-              <div>
-                <h2 className="text-2xl text-gray-800">Chatroom Members:</h2>
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    console.log("search");
-                  }}
-                  className="group relative h-10 w-full"
-                >
-                  <label className="absolute top-3 left-3 flex items-center justify-center text-gray-400">
-                    <button
-                      type="submit"
-                      className="h-full w-full cursor-default"
-                    >
-                      <IoSearchOutline className="group-focus-within:text-secondary group-hover:text-secondary h-6 w-6 text-gray-400" />
-                    </button>
-                  </label>
-                  <TextInput
-                    name="search"
-                    placeholder="Search for a member"
-                    onChange={(e) => handleSearchChange(e)}
-                    inputClassName="pl-12 py-[8px] "
-                  />
-                </form>
-                <ul className="no-scrollbar mt-4 gap-y-1 flex flex-col overflow-y-scroll h-[calc(60vh-160px)] scroll-smooth">
-                  {!searchError &&
-                    !searchLoading &&
-                    searchResults &&
-                    searchResults?.map((member: IRoomMember, index: number) => (
-                      <MemberListItem key={index} member={member} />
-                    ))}
-                  {searchLoading &&
-                    [...new Array(6)].map((i) => (
-                      <UserListItemLoading key={i} />
-                    ))}
-                  {!searchError && searchResults?.length === 0 && (
-                    <p className="py-10 text-center text-gray-400">
-                      No results found
-                    </p>
-                  )}
-                </ul>
-              </div>
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
+      <div className=" max-h-[1000px] w-[800px]">
+        <div className="flex justify-between items-center h-1/3">
+          {CurrentUser.membershipStatus === MembershipStatus.OWNER && (
+            <OwnerPanel roomData={roomData} />
+          )}
+          {CurrentUser.membershipStatus === MembershipStatus.MODERATOR && (
+            <AdminPanel roomData={roomData} />
+          )}
+          {CurrentUser.membershipStatus === MembershipStatus.MEMBER && (
+            <MemberPanel />
+          )}
+        </div>
+        <RoomMembers />
+      </div>
+      <div className="w-full flex flex-row justify-around items-center">
+        <Button variant="danger">Delete Room</Button>
+        <Button
+          variant="primary"
+          type="submit"
+          isLoading={isLoading}
+          onClick={handleSave}
+        >
+          {buttonText}
+        </Button>
       </div>
     </BaseModal>
   );
